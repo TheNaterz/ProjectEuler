91252f205f3fd4b4fa475d7f83cfbe6968cca439
TheNaterz <natejcaroe@gmail.com>
2015-12-03 12:29:16 -0700

    easy

---
+++ 25.rb
@@ -0,0 +1,34 @@
+=begin 
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+Hence the first 12 terms will be:
+
+F1 = 1
+F2 = 1
+F3 = 2
+F4 = 3
+F5 = 5
+F6 = 8
+F7 = 13
+F8 = 21
+F9 = 34
+F10 = 55
+F11 = 89
+F12 = 144
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+=end
+
+a = 1
+b = 1
+(3..5000).each do |x|
+  a, b = b, a + b
+  if b.to_s.split('').length >= 1000
+    puts x
+    break
+  end
+end
